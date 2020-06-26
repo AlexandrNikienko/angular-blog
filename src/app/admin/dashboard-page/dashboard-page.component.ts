f4a5045369938bf5237a407b2f73ef81c9e4415f
anikienko <anikienko.callfire.com>
2020-06-26 22:12:42 +0300

    admin dashboard - blogs list

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/shared/services/posts.service';
+import { Post } from './../../shared/interfaces';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-dashboard-page',
-  templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+	selector: 'app-dashboard-page',
+	templateUrl: './dashboard-page.component.html',
+	styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+	public posts: Post[] = [];
+	private postsSubscription: Subscription;
 
-  constructor() { }
+	constructor(
+		private postsServise: PostsService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.postsSubscription = this.postsServise.getAll()
+			.subscribe((response) => {
+				this.posts = response;
+			})
+	}
 
+	remove(id: string): void {
+
+	}
+
+	ngOnDestroy(): void {
+		this.postsSubscription.unsubscribe();
+	}
 }
